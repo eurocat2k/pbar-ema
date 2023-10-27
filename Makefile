8cba60e93c2307572b451881f38b7cb3590c45d5
eurocat2k <gabor.zelenak@me.com>
2023-10-27 09:18:36 +0000

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,13 +1,18 @@
 CC=clang
-CFLAGS=-g -O2 -Wall -Wextra -I.
+CFLAGS=-O2 -Wall -Wextra -I.
+CFLAGS_DEBUG=-g -O2 -Wall -Wextra -I. -DDEBUG
 LDFLAGS=-lc
+SRCS=pbar.c
 
 BIN=pbar
+BINDBG=pbar.dbg
 
-all: $(BIN)
+all: $(BIN) $(BINDBG)
 
-%: %.c
-	$(CC) $< -o $(BIN) $(LDFLAGS)
-
+$(BIN): $(SRCS)
+	$(CC) $(CFLAGS) $(SRCS) -o $(BIN) $(LDFLAGS)
+$(BINDBG): $(SRCS)
+	$(CC) $(CFLAGS_DEBUG) $(SRCS) -o $(BIN).dbg $(LDFLAGS)
 clean:
-	@rm $(BIN)
+	@rm $(BIN) 2>&1 >/dev/null
+	@rm $(BIN).dbg 2>&1 >/dev/null
--- pbar.c
+++ pbar.c
@@ -14,89 +14,107 @@ typedef struct {
   long total_urls;
 } statusinfo;
 
-const double PREDICT_WEIGHT = 0.4;
-
-double predict_next(double last_prediction, double actual) {
-  return ((last_prediction * (1.0 - PREDICT_WEIGHT)) + (actual * PREDICT_WEIGHT));
-}
-
 typedef struct {
   size_t bytes;
 } dataentry;
 
+const double PREDICT_WEIGHT = 0.3;
+
+void update_pbar(int p, statusinfo *sinfo);
+double predict_next(double last_prediction, double actual);
+size_t got_data(dataentry *ent, statusinfo *sinfo);
+
+double predict_next(double last_prediction, double actual) {
+  double predict = ((last_prediction * (1.0 - PREDICT_WEIGHT)) + (actual * PREDICT_WEIGHT));
+#ifdef DEBUG
+  fprintf(stdout, "  prediction> last: %f, actual: %f, new: %f\n", last_prediction, actual, predict);
+#endif
+  return predict;
+}
+
+#ifndef DEBUG
 void update_pbar(int p, statusinfo *sinfo) {
-	fprintf(stdout, "\r[");
+	printf("\r[");
 	int nleft = p;
 	int numch = (PROG_BAR_LENGTH * p) / 100;
 	for (int i = 0; i < PROG_BAR_LENGTH; i++) {
 		if (numch-- > 0) {
-			fprintf(stdout,"#");
+			printf("#");
 		} else {
-			fprintf(stdout, " ");
+			printf(" ");
 		}
 	}
-	fprintf(stdout, "] %3d %% *(bytes: %ld)", p, sinfo->total_bytes);
+	printf("] %3d %% (bytes: %ld)", p, sinfo->total_bytes);
 	fflush(stdout);
 }
+#else
+void update_pbar(int p, statusinfo *sinfo) {return;}
+#endif
 
 size_t got_data(dataentry *ent, statusinfo *sinfo) {
     size_t bytes = 0;
-    for (size_t i = 0; i < ent->bytes; i++) {
-		sinfo->total_bytes += i;
-		sinfo->current_bytes = bytes;
-		bytes = i;
-		long urls_left = sinfo->total_urls - sinfo->url_so_far;
-		long estimated_current = sinfo->exp_bytes_per_url;
-		if (sinfo->current_bytes > sinfo->exp_bytes_per_url) {
-				estimated_current = sinfo->current_bytes * 4 / 3;
-		}
-		double guess_next_prediction =
-			predict_next(sinfo->exp_bytes_per_url, estimated_current);
-		long estimated_total =
-			sinfo->total_bytes +
-			(estimated_current - sinfo->current_bytes) +
-			((urls_left - 1) * guess_next_prediction);
-
-		long percentdone = sinfo->total_bytes * 100 / estimated_total;
-		// usleep(120);
-		update_pbar(percentdone, sinfo);
+    sinfo->current_bytes += ent->bytes;
+    bytes += ent->bytes;
+    long urls_left = sinfo->total_urls - sinfo->url_so_far;
+    long estimated_current = sinfo->exp_bytes_per_url;
+    if (sinfo->current_bytes > sinfo->exp_bytes_per_url) {
+        estimated_current = sinfo->current_bytes * (4 / 3);
     }
-    // long urls_left = sinfo->total_urls - sinfo->url_so_far;
-    // long estimated_current = sinfo->exp_bytes_per_url;
-    // if (sinfo->current_bytes > sinfo->exp_bytes_per_url) {
-    //     estimated_current = sinfo->current_bytes * 4 / 3;
-    // }
-    // double guess_next_prediction = predict_next(sinfo->exp_bytes_per_url, estimated_current);
-    // long estimated_total = sinfo->total_bytes + (estimated_current - sinfo->current_bytes) + ((urls_left - 1) * guess_next_prediction);
+    double guess_next_prediction = predict_next(sinfo->exp_bytes_per_url, estimated_current);
+    long estimated_total = sinfo->total_bytes + (estimated_current - sinfo->current_bytes) + ((urls_left - 1) * guess_next_prediction);
 
-    // long percentdone = sinfo->total_bytes * 100 / estimated_total;
-	// // usleep(120);
-    // update_pbar(percentdone, sinfo);
+    if (urls_left) {
+      // align expected final
+      double est_final = predict_next((double) sinfo->total_bytes, (double)estimated_total);
+      if (est_final > sinfo->total_bytes) {
+	estimated_total = (long)est_final;
+      }
+    }
+
+    long percentdone = sinfo->total_bytes * 100 / estimated_total;
+#ifdef DEBUG
+    fprintf(stderr, " [[urls_left: %ld, total: %ld, current: %ld, expected: %ld, guessed: %f, estimated: %ld, progress: %d %%]]\n", urls_left, sinfo->total_bytes, sinfo->current_bytes, estimated_current, guess_next_prediction, estimated_total, percentdone);
+#endif    
+    usleep(100);
+    update_pbar(percentdone, sinfo);
     return bytes;
 }
 
 bool get_data_entry(dataentry *ent, statusinfo *sinfo) {
-    got_data(ent, sinfo);
-    usleep(500000);
+    //sinfo->total_bytes += ent->bytes;
+    sinfo->current_bytes = 0;
+    dataentry byte = {.bytes = 1};
+    for (size_t i = 0; i < ent->bytes; i++) {
+      size_t size = got_data(&byte, sinfo);
+      sinfo->total_bytes += size;
+#ifdef DEBUG
+      fprintf(stderr, "entry: {.bytes = %ld }\n", ent->bytes);
+#else
+      //usleep(10000);
+#endif
+    }
     return true;
 }
 
 int main() {
-	dataentry data[] = {{.bytes = 21234}, {.bytes = 34324}, {.bytes = 22134}, {.bytes =114435}, {.bytes = 27643}};
-	statusinfo sinfo = {0};
+    dataentry data[] = {{.bytes = 21234}, {.bytes = 34324}, {.bytes = 22134}, {.bytes =114435}, {.bytes = 27643}};
+    statusinfo sinfo = {0};
     int num_entries = sizeof(data) / sizeof(data[0]);
-    sinfo.total_urls = num_entries - 1;
-    sinfo.exp_bytes_per_url = 10000000;
+    sinfo.total_urls = num_entries;
+    sinfo.exp_bytes_per_url = 1000;
     update_pbar(0, &sinfo);
     for (int i = 0; i < num_entries; i++) {
-        // update_pbar(i);
-        // usleep(20000);
         sinfo.current_bytes = 0;
-		get_data_entry(&data[i], &sinfo);
-		// update_pbar(((i + 1) * 100) / num_entries, &sinfo);
-		sinfo.url_so_far++;
-		sinfo.exp_bytes_per_url = predict_next(sinfo.exp_bytes_per_url, sinfo.current_bytes);
+	sinfo.url_so_far;
+	get_data_entry(&data[i], &sinfo);
+	//sinfo.url_so_far++;
+	sinfo.exp_bytes_per_url = predict_next(sinfo.exp_bytes_per_url, sinfo.current_bytes);
+	sinfo.url_so_far += 1;
+#ifdef DEBUG
+	fprintf(stderr, "- sinfo: {.current_bytes = %ld, .url_so_far = %ld, .exp_bytes_per_url = %f}\n",sinfo.current_bytes, sinfo.url_so_far, sinfo.exp_bytes_per_url);
+#endif
     }
-    fprintf(stdout, " Done\n");
-	return 0;
+    update_pbar(100, &sinfo);
+    fprintf(stdout, " Done [total: %ld]\n", sinfo.total_bytes);
+    return 0;
 }
